a9f353b59016097881e92d2a5fae73caa09e6f2a
Tomáš Záruba <zaruba@skunks.cz>
2024-10-04 19:29:10 +0200

    no message

--- src/transformation/kindly_reminder/runner.cron.ts
+++ src/transformation/kindly_reminder/runner.cron.ts
@@ -1,14 +1,12 @@
 import { CronJob } from "encore.dev/cron";
 import { api } from "encore.dev/api";
-
-import {transformation_kindly_reminder, my_service_one} from "~encore/clients";
-
-
+import moment from "moment";
+import {transformation_kindly_reminder} from "~encore/clients";
 
 
 export const cronValidWeek = api({expose: false}, async (): Promise<void> => {
     await transformation_kindly_reminder.run_validator_script (
-        { value: 40 }
+        { value: moment().week() }
     );
 });
 
