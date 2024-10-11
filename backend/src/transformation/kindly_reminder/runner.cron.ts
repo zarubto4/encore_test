5c3884d86518f92af0b575991c5cc70598efda7c
Tomáš Záruba <zaruba@skunks.cz>
2024-10-11 09:04:10 +0200

    no message

--- backend/src/transformation/kindly_reminder/runner.cron.ts
+++ backend/src/transformation/kindly_reminder/runner.cron.ts
@@ -2,11 +2,20 @@ import { CronJob } from "encore.dev/cron";
 import { api } from "encore.dev/api";
 import moment from "moment";
 import { transformation_kindly_reminder } from "~encore/clients";
+import log from "encore.dev/log";
 
 // --- Valid Week ------------------------------------------------------------------------------------------------------
 
 export const cronValidWeek = api({ expose: false }, async (): Promise<void> => {
-  await transformation_kindly_reminder.runValidatorScript({ value: moment().week() });
+  await transformation_kindly_reminder
+    .runValidatorScript({ value: moment().week() })
+    .then(() => {
+      log.info("cronValidWeek done");
+    })
+    .catch((error) => {
+      log.error("cronValidWeek failed", error);
+      log.error("cronValidWeek failed: message", error.message);
+    });
 });
 
 new CronJob("transformation-kindlyReminder-weeklyChecker", {
@@ -18,10 +27,18 @@ new CronJob("transformation-kindlyReminder-weeklyChecker", {
 // --- Close Asana task from last week ---------------------------------------------------------------------------------
 
 export const cronCloseAsanaTaskFromLastWeek = api({ expose: false }, async (): Promise<void> => {
-  await transformation_kindly_reminder.runGeneratorScript({
-    name: "close_asana_tickets",
-    week: moment().week() - 1,
-  });
+  await transformation_kindly_reminder
+    .runGeneratorScript({
+      name: "close_asana_tickets",
+      week: moment().week() - 1,
+    })
+    .then(() => {
+      log.info("cronCloseAsanaTaskFromLastWeek done");
+    })
+    .catch((error) => {
+      log.error("cronCloseAsanaTaskFromLastWeek failed", error);
+      log.error("cronCloseAsanaTaskFromLastWeek failed: message", error.message);
+    });
 });
 
 new CronJob("transformation-kindlyReminder-closeAsanaPreviousWeek", {
@@ -33,10 +50,18 @@ new CronJob("transformation-kindlyReminder-closeAsanaPreviousWeek", {
 // --- Generate next week -----------------------------------------------------------------------------------------------
 
 export const generateNextWeekOfIssues = api({ expose: false }, async (): Promise<void> => {
-  await transformation_kindly_reminder.runGeneratorScript({
-    name: "run_whole_script",
-    week: moment().week(),
-  });
+  await transformation_kindly_reminder
+    .runGeneratorScript({
+      name: "run_whole_script",
+      week: moment().week(),
+    })
+    .then(() => {
+      log.info("generateNextWeekOfIssues done");
+    })
+    .catch((error) => {
+      log.error("generateNextWeekOfIssues failed", error);
+      log.error("generateNextWeekOfIssues failed: message", error.message);
+    });
 });
 
 new CronJob("transformation-kindlyReminder-generateNextWeekOfIssues", {
