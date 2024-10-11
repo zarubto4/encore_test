35b61b34fba6882dfabd3605b61cc74b084c3e84
Tomáš Záruba <zaruba@skunks.cz>
2024-10-11 08:56:50 +0200

    no message

--- backend/src/system_1/service_1/encore.service.ts
+++ backend/src/system_1/service_1/encore.service.ts
@@ -1,6 +1,2 @@
 import { Service } from "encore.dev/service";
-import log from "../../../../../../../../../opt/homebrew/Cellar/encore/1.41.8/libexec/runtimes/js/encore.dev/log/mod";
-import { service } from "../../transformation/kindly_reminder/encore.service";
-
 export default new Service("my_service_one");
-log.info("Running service " + service.name);
